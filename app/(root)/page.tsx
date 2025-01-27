de6b93e044ae10b03dc3026b781701367a54eeed
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2025-01-27 15:22:28 +0100

    base styles for hero component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,33 +6,13 @@ import HotelCard from "@/components/HotelCard";
 import Searchbar from "@/components/Searchbar";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import HeroImage from "../../public/images/heroimage.jpg";
 
 const Home = () => {
   return (
-    <div className="bg-gray-100">
-      {/* Hero Section */}
-      <section
-        className="relative bg-cover bg-center h-screen"
-        style={{ backgroundImage: "url('/path-to-your-hero-image.jpg')" }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <div className="text-center text-white space-y-6 px-4">
-            <h1 className="text-4xl md:text-6xl font-bold">
-              Linking You to Hassle-Free Hotel Stays
-            </h1>
-            <p className="text-lg md:text-xl">
-              Find your perfect stay with no booking fees or middlemen.
-            </p>
-            <div>
-              <button className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700">
-                Explore Hotels
-              </button>
-              <button className="px-6 py-3 bg-gray-200 text-gray-800 rounded-lg shadow-md hover:bg-gray-300 ml-4">
-                Learn More
-              </button>
-            </div>
-          </div>
-        </div>
+    <div>
+      <section>
+        <Hero />
       </section>
 
       {/* Features Section */}
