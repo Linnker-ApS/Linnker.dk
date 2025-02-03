e92a6ae549db3fa545e9d5a5851097fbfd1f0ec3
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2025-02-03 23:29:57 +0100

    Linnker logo setup

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-black text-white w-full py-8">
       <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold mb-4">Linnker</h1>
         <div>Footer</div>
       </div>
     </footer>
